d26ac6cef1b082bf4b95251cb85b5d1936b9eb5a
barakbarlevi <barakbarlevi@gmail.com>
2025-07-23 04:12:13 -0400

    Documenting

--- shared/src/core/crc8.d
+++
@@ -1,4 +0,0 @@
-../shared/src/core/crc8.o: ../shared/src/core/crc8.c \
- ../shared/inc/core/crc8.h inc/common-defines.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h
--- shared/src/core/simple-timer.d
+++
@@ -1,5 +0,0 @@
-../shared/src/core/simple-timer.o: ../shared/src/core/simple-timer.c \
- ../shared/inc/core/system.h inc/common-defines.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
- ../shared/inc/core/simple-timer.h
